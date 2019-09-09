6e7207963d33c4de96d66c11c32521c2dcaee410
zhuang <289590076@qq.com>
2019-09-10 01:46:17 +0800

    调用cmd

--- src/test/java/com/zhuang/SpringbootMybatisApplicationTests.java
+++ src/test/java/com/zhuang/SpringbootMybatisApplicationTests.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -23,6 +26,41 @@ public class SpringbootMybatisApplicationTests {
 
         List<Commentt> commentList = commenttMapper.getCommentList(1);
         System.out.println(commentList);
+
+    }
+
+
+
+    @Test
+    public void testProcessBuilder(){
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("ping", "127.0.0.1");
+        //将标准输入流和错误流合并，通过标准输入流读取信息
+        processBuilder.redirectErrorStream(true);
+
+        try {
+            //启动进程
+            Process start = processBuilder.start();
+            //获取输入流
+            InputStream inputStream = start.getInputStream();
+            //转成字符输出流
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "gbk");
+
+            int len = -1;
+            char[] c = new char[1024];
+            StringBuffer outputString = new StringBuffer();
+            //读取进程输入流中的内容
+            while ((len = inputStreamReader.read(c)) != -1){
+                String s = new String(c, 0, len);
+                outputString.append(s);
+                System.out.println(s);
+            }
+            inputStream.close();
+            inputStreamReader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
